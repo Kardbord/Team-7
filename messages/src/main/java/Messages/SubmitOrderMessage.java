c35482f3f0f751b9dcbd983f4cfbb59d6cb08d87
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2019-02-19 09:31:52 -0700

    Add SubmitOrderMessage (#6)
    * Implemente SubmitOrderMessage
    
    * Refactored SubmitOrderMessage class to use an enumeration for order type
    
    * Implemented SubmitOrderMessageTest
    
    * Refactored OrderType enum to use ordinals for byte values
    
    * Fixed bounds checking in SubmitOrderMessage.OrderType::contains

---
+++ messages/src/main/java/Messages/SubmitOrderMessage.java
@@ -0,0 +1,93 @@
+package Messages;
+
+import java.io.IOException;
+
+public class SubmitOrderMessage extends Message {
+
+    private short playerId;
+    private OrderType orderType;
+    private short quantity;
+    private short price;
+    private String symbol;
+
+    public SubmitOrderMessage(short playerId, OrderType orderType, short quantity, short price, String symbol) {
+        super(MessageType.SUBMIT_ORDER);
+        this.playerId = playerId;
+        this.orderType = orderType;
+        this.quantity = quantity;
+        this.price = price;
+        this.symbol = symbol;
+    }
+
+    public static SubmitOrderMessage decode(byte[] messageBytes) {
+        Decoder decoder = new Decoder(messageBytes);
+
+        if (decoder.decodeMessageType() != MessageType.SUBMIT_ORDER) {
+            throw new IllegalArgumentException();
+        }
+
+        short playerId = decoder.decodeShort();
+
+        byte orderTypeByte = decoder.decodeByte();
+        if (!OrderType.contains(orderTypeByte)) {
+            throw new IllegalArgumentException();
+        }
+
+        short quantity = decoder.decodeShort();
+        short price = decoder.decodeShort();
+        String symbol = decoder.decodeString();
+
+        return new SubmitOrderMessage(playerId, OrderType.getOrderTypeFromByte(orderTypeByte), quantity, price, symbol);
+    }
+
+    @Override
+    public byte[] encode() throws IOException {
+        return new Encoder()
+                .encodeMessageType(messageType)
+                .encodeShort(playerId)
+                .encodeByte(orderType.toByte())
+                .encodeShort(quantity)
+                .encodeShort(price)
+                .encodeString(symbol)
+                .toByteArray();
+    }
+
+    public short getPlayerId() {
+        return playerId;
+    }
+
+    public short getQuantity() {
+        return quantity;
+    }
+
+    public short getPrice() {
+        return price;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public OrderType getOrderType() {
+        return orderType;
+    }
+
+    public enum OrderType {
+        SELL,
+        BUY,
+        ;
+
+
+        public byte toByte() {
+            return (byte) this.ordinal();
+        }
+
+        public static OrderType getOrderTypeFromByte(byte orderTypeByte) {
+            return OrderType.values()[orderTypeByte];
+        }
+
+        public static boolean contains(byte orderTypeByte) {
+            return (orderTypeByte >= 0 && orderTypeByte < OrderType.values().length);
+        }
+    }
+}
---
+++ messages/src/test/java/Messages/SubmitOrderMessageTest.java
@@ -0,0 +1,68 @@
+package Messages;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class SubmitOrderMessageTest {
+
+    @Test
+    public void encodesIntoExpected() throws IOException {
+        Message.MessageType expectedMessageType = Message.MessageType.SUBMIT_ORDER;
+        short expectedPlayerId = 12;
+        SubmitOrderMessage.OrderType expectedOrderType = SubmitOrderMessage.OrderType.BUY;
+        short expectedQuantity = 42;
+        short expectedPrice = 1200;
+        String expectedSymbol = "NVDA";
+
+        byte[] expectedMessageBytes = new Message.Encoder()
+                .encodeMessageType(expectedMessageType)
+                .encodeShort(expectedPlayerId)
+                .encodeByte(expectedOrderType.toByte())
+                .encodeShort(expectedQuantity)
+                .encodeShort(expectedPrice)
+                .encodeString(expectedSymbol)
+                .toByteArray();
+
+        byte[] actualMessageBytes =
+                new SubmitOrderMessage(
+                        expectedPlayerId,
+                        expectedOrderType,
+                        expectedQuantity,
+                        expectedPrice,
+                        expectedSymbol).encode();
+
+        assertArrayEquals(expectedMessageBytes, actualMessageBytes);
+    }
+
+    @Test
+    public void decodesIntoExpected() throws IOException {
+        Message.MessageType expectedMessageType = Message.MessageType.SUBMIT_ORDER;
+        short expectedPlayerId = 12;
+        SubmitOrderMessage.OrderType expectedOrderType = SubmitOrderMessage.OrderType.BUY;
+        short expectedQuantity = 42;
+        short expectedPrice = 1200;
+        String expectedSymbol = "NVDA";
+
+        byte[] messageBytes = new Message.Encoder()
+                .encodeMessageType(expectedMessageType)
+                .encodeShort(expectedPlayerId)
+                .encodeByte(expectedOrderType.toByte())
+                .encodeShort(expectedQuantity)
+                .encodeShort(expectedPrice)
+                .encodeString(expectedSymbol)
+                .toByteArray();
+
+        SubmitOrderMessage victim = SubmitOrderMessage.decode(messageBytes);
+
+        assertEquals(expectedMessageType, victim.getMessageType());
+        assertEquals(expectedPlayerId, victim.getPlayerId());
+        assertEquals(expectedOrderType, victim.getOrderType());
+        assertEquals(expectedQuantity, victim.getQuantity());
+        assertEquals(expectedPrice, victim.getPrice());
+        assertEquals(expectedSymbol, victim.getSymbol());
+    }
+}
