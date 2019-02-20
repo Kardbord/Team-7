21ef5abae63d87f5f565da9ec17ea53f8ee644c7
Paxton <palexander2113@gmail.com>
2019-02-20 13:38:12 -0700

    Added CancelOrderMessage and Unit test for it

---
+++ messages/src/main/java/Messages/CancelOrderMessage.java
@@ -0,0 +1,50 @@
+package Messages;
+
+import java.io.IOException;
+
+public class CancelOrderMessage extends Message {
+
+    private short playerId;
+    private short orderId;
+    private String symbol;
+
+    public CancelOrderMessage(short playerId, short orderId, String symbol) {
+        super(MessageType.CANCEL_ORDER);
+        this.playerId = playerId;
+        this.orderId = orderId;
+        this.symbol = symbol;
+    }
+
+    public static CancelOrderMessage decode(byte[] messageBytes) {
+        Decoder decoder = new Decoder(messageBytes);
+
+        if (decoder.decodeMessageType() != MessageType.CANCEL_ORDER) {
+            throw new IllegalArgumentException();
+        }
+
+        short playerId = decoder.decodeShort();
+
+        short orderId = decoder.decodeShort();
+        String symbol = decoder.decodeString();
+
+        return new CancelOrderMessage(playerId, orderId, symbol);
+    }
+
+    @Override
+    public byte[] encode() throws IOException {
+        return new Encoder()
+                .encodeMessageType(messageType)
+                .encodeShort(playerId)
+                .encodeShort(orderId)
+                .encodeString(symbol)
+                .toByteArray();
+    }
+
+    public short getPlayerId() {
+        return playerId;
+    }
+
+    public short getOrderId() { return orderId; }
+
+    public String getSymbol() { return symbol; }
+}
---
+++ messages/src/test/java/Messages/CancelOrderMessageTest.java
@@ -0,0 +1,56 @@
+package Messages;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class CancelOrderMessageTest {
+
+    @Test
+    public void encodesIntoExpected() throws IOException {
+        Message.MessageType expectedMessageType = Message.MessageType.CANCEL_ORDER;
+        short expectedPlayerId = 12;
+        short expectedOrderId = 42;
+        String expectedSymbol = "NVDA";
+
+        byte[] expectedMessageBytes = new Message.Encoder()
+                .encodeMessageType(expectedMessageType)
+                .encodeShort(expectedPlayerId)
+                .encodeShort(expectedOrderId)
+                .encodeString(expectedSymbol)
+                .toByteArray();
+
+        byte[] actualMessageBytes =
+                new CancelOrderMessage(
+                        expectedPlayerId,
+                        expectedOrderId,
+                        expectedSymbol).encode();
+
+        assertArrayEquals(expectedMessageBytes, actualMessageBytes);
+    }
+
+    @Test
+    public void decodesIntoExpected() throws IOException {
+        Message.MessageType expectedMessageType = Message.MessageType.CANCEL_ORDER;
+        short expectedPlayerId = 12;
+        short expectedOrderId = 42;
+        String expectedSymbol = "NVDA";
+
+        byte[] messageBytes = new Message.Encoder()
+                .encodeMessageType(expectedMessageType)
+                .encodeShort(expectedPlayerId)
+                .encodeShort(expectedOrderId)
+                .encodeString(expectedSymbol)
+                .toByteArray();
+
+        CancelOrderMessage victim = CancelOrderMessage.decode(messageBytes);
+
+        assertEquals(expectedMessageType, victim.getMessageType());
+        assertEquals(expectedPlayerId, victim.getPlayerId());
+        assertEquals(expectedOrderId, victim.getOrderId());
+        assertEquals(expectedSymbol, victim.getSymbol());
+    }
+}
