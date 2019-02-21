f28cf186709c6304e9a82a38f7fc0304f2efe078
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2019-02-20 20:52:05 -0700

    Add TopOfBookResponseMessage (#18)
    * Implemented TopOfBookResponseMessage
    
    * Implemented TopOfBookResponseMessageTest

---
+++ messages/src/test/java/messages/TopOfBookResponseMessageTest.java
@@ -0,0 +1,66 @@
+package messages;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class TopOfBookResponseMessageTest {
+
+    @Test
+    public void encodesIntoExpected() throws IOException {
+        Message.MessageType expectedMessageType = Message.MessageType.TOP_OF_BOOK_RESPONSE;
+        String expectedSymbol = "NVDA";
+        short expectedBidPrice = 500;
+        short expectedBidQty = 5;
+        short expectedAskPrice = 550;
+        short expectedAskQty = 3;
+
+        byte[] expectedMessageBytes = new Message.Encoder()
+                .encodeMessageType(expectedMessageType)
+                .encodeString(expectedSymbol)
+                .encodeShort(expectedBidPrice)
+                .encodeShort(expectedBidQty)
+                .encodeShort(expectedAskPrice)
+                .encodeShort(expectedAskQty)
+                .toByteArray();
+
+        byte[] actualMessageBytes =
+                new TopOfBookResponseMessage(
+                        expectedSymbol,
+                        expectedBidPrice,
+                        expectedBidQty, expectedAskPrice,
+                        expectedAskQty).encode();
+
+        assertArrayEquals(expectedMessageBytes, actualMessageBytes);
+    }
+
+    @Test
+    public void decodesIntoExpected() throws IOException {
+        Message.MessageType expectedMessageType = Message.MessageType.TOP_OF_BOOK_RESPONSE;
+        String expectedSymbol = "NVDA";
+        short expectedBidPrice = 500;
+        short expectedBidQty = 5;
+        short expectedAskPrice = 550;
+        short expectedAskQty = 3;
+
+        byte[] messageBytes = new Message.Encoder()
+                .encodeMessageType(expectedMessageType)
+                .encodeString(expectedSymbol)
+                .encodeShort(expectedBidPrice)
+                .encodeShort(expectedBidQty)
+                .encodeShort(expectedAskPrice)
+                .encodeShort(expectedAskQty)
+                .toByteArray();
+
+        TopOfBookResponseMessage victim = TopOfBookResponseMessage.decode(messageBytes);
+
+        assertEquals(expectedSymbol, victim.getSymbol());
+        assertEquals(expectedBidPrice, victim.getBidPrice());
+        assertEquals(expectedBidQty, victim.getBidQuantity());
+        assertEquals(expectedAskPrice, victim.getAskPrice());
+        assertEquals(expectedAskQty, victim.getAskQuantity());
+    }
+
+}
