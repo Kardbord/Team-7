46d273534e8ebf6457f3bb84aa93638fded7a2c9
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2019-03-03 23:47:11 -0700

    Added App class to instantiate a Gateway (#29)
    * Added App class to instantiate a Gateway
    
    * Removed duplicates in exchange-gateway/pom.xml

---
+++ exchange-gateway/src/main/java/gateway/App.java
@@ -0,0 +1,9 @@
+package gateway;
+
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        Gateway gateway = new Gateway();
+    }
+}
